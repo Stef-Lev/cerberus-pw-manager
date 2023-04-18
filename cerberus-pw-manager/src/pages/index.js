d18c0801119ec93a3c8ce6e98cbf131f419da6aa
Stefanos Leventis <sigianas@yahoo.com>
2023-04-18 23:12:32 +0300

    find bug with smooth scrolling and showMsg

--- cerberus-pw-manager/src/pages/index.js
+++ cerberus-pw-manager/src/pages/index.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState, useContext } from "react";
 import ScrollTopButton from "../components/ScrollTopButton";
 import { getAllMethod } from "../helpers/services";
 import { useSession } from "next-auth/react";
-// import { UserContext } from "../context/UserContext";
 import { Flex, Box } from "@chakra-ui/react";
 import RecordItem from "../components/RecordItem";
 import TopNav from "../components/TopNav";
@@ -10,27 +9,25 @@ import Loader from "../components/Loader";
 
 function Home() {
   const [records, setRecords] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
   const { data: session, loading } = useSession();
-  // const { user } = useContext(UserContext);
 
   useEffect(() => {
     let mounted = true;
-    getAllMethod(`/api/user/${session?.user.id}/records`)
-      .then((result) => {
-        if (mounted) {
-          console.log("result", result);
-          setRecords(result);
-        }
-      })
-      .then(() => setLoading(false))
-      .catch((err) => console.log(err));
+    if (session) {
+      getAllMethod(`/api/user/${session?.user.id}/records`)
+        .then((result) => {
+          if (mounted) {
+            console.log("result", result);
+            setRecords(result);
+          }
+        })
+        .catch((err) => console.log(err));
+    }
+
     return () => {
       mounted = false;
     };
   }, [session]);
-
-  console.log("session", session);
 
   return (
     <Box>
