f4836fd0c92c18a47fa764190c8ee50ff48ab6db
Stefanos Leventis <sigianas@yahoo.com>
2023-06-13 23:53:42 +0300

    add 404 and error page

---
+++ src/pages/404.tsx
@@ -0,0 +1,8 @@
+import { Box } from "@chakra-ui/react";
+import Layout from "@/components/Layout";
+
+const page404 = () => {
+  return <Box pt="60px">STEF</Box>;
+};
+
+export default page404;
---
+++ src/pages/error.tsx
@@ -0,0 +1,5 @@
+const error = () => {
+  return <div>Enter</div>;
+};
+
+export default error;
