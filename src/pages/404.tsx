9efde59259ff2c74397b1f33c356eb9c3a215cf6
Stefanos Leventis <sigianas@yahoo.com>
2023-06-15 23:24:49 +0300

    add error page and 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,10 +1,31 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Flex, useBreakpointValue, Text, Heading } from "@chakra-ui/react";
+import Button from "@/components/Button";
+import Icon404 from "@/components/Icon404";
 import { useRouter } from "next/router";
 
 const page404 = () => {
+  const iconSize = useBreakpointValue({ base: 320, sm: 360, md: 440 });
   const router = useRouter();
-  console.log(router.pathname);
-  return <Box pt="60px">STEF</Box>;
+  return (
+    <Box pt="60px">
+      <Flex alignItems="center" flexDirection="column">
+        <Heading fontSize={32} mb="16px">
+          Error
+        </Heading>
+        <Icon404 sideSize={iconSize} />
+        <Text fontSize={26} mt="16px">
+          Page not found
+        </Text>
+        <Button
+          buttonType="primary"
+          marginTop="16px"
+          onClick={() => router.push("/")}
+        >
+          Go back home
+        </Button>
+      </Flex>
+    </Box>
+  );
 };
 
 export default page404;
--- src/pages/error.tsx
+++ src/pages/error.tsx
@@ -1,5 +1,31 @@
-const error = () => {
-  return <div>Enter</div>;
+import { Box, Flex, useBreakpointValue, Text, Heading } from "@chakra-ui/react";
+import Button from "@/components/Button";
+import Icon500 from "@/components/Icon500";
+import { useRouter } from "next/router";
+
+const pageError = () => {
+  const iconSize = useBreakpointValue({ base: 320, sm: 360, md: 440 });
+  const router = useRouter();
+  return (
+    <Box pt="60px">
+      <Flex alignItems="center" flexDirection="column">
+        <Heading fontSize={32} mb="16px">
+          Error
+        </Heading>
+        <Icon500 sideSize={iconSize} />
+        <Text fontSize={26} mt="16px">
+          Something went wrong
+        </Text>
+        <Button
+          buttonType="primary"
+          marginTop="16px"
+          onClick={() => router.push("/")}
+        >
+          Go back home
+        </Button>
+      </Flex>
+    </Box>
+  );
 };
 
-export default error;
+export default pageError;
